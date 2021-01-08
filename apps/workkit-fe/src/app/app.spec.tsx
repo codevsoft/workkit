e72a30dd993c673b1bdf08ef305340274f26c90a
Javier Moreno <moreno.jml88@gmail.com>
2021-01-08 18:40:40 +0100

    updated tests

--- apps/workkit-fe/src/app/app.spec.tsx
+++ apps/workkit-fe/src/app/app.spec.tsx
@@ -1,28 +1,13 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import { rest } from "msw";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-import App, { Todo } from "./app";
+import App from './app';
 
-const todos: Todo[] = [{ title: "Todo 1" }, { title: "Todo 2" }];
+describe('App', () => {
+	it('should render successfully', () => {
+		const { baseElement } = render(<App />);
 
-export const handlers = [
-  // Handles a GET /user request
-  rest.get("/api/todos", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(todos));
-  }),
-];
-
-describe("App", () => {
-  it("should render successfully", () => {
-    const { baseElement } = render(<App />);
-
-    expect(baseElement).toBeTruthy();
-  });
-
-  it("should render the correct title", () => {
-    render(<App />);
-    expect(screen.getByText("Welcome to workkit!")).toBeTruthy();
-  });
+		expect(baseElement).toBeTruthy();
+	});
 });
--- libs/page-login/src/lib/page-login.spec.tsx
+++ libs/page-login/src/lib/page-login.spec.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { render } from '@testing-library/react';
 
-import PageLogin from "./page-login";
+import { PageLogin } from './page-login';
 
-describe("PageLogin", () => {
-  it("should render successfully", () => {
-    const { baseElement } = render(<PageLogin />);
-    expect(baseElement).toBeTruthy();
-  });
+describe('PageLogin', () => {
+	it('should render successfully', () => {
+		const { baseElement } = render(<PageLogin />);
+		expect(baseElement).toBeTruthy();
+	});
 });
