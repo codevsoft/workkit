e7fc032cbfc4b2c105624bc29be907c3a7a8551a
Sergio Vazquez Vicente <sergivv@gmail.com>
2021-01-20 12:14:18 +0100

    Add internationalization with i18next (#16)
    * Add internationalization with i18next
    
    * Change default language
    
    * Update libs/page-login/src/lib/page-login.tsx
    
    Co-authored-by: José Ignacio Amelivia Santiago <ohcan2@gmail.com>
    
    * Add Strict mode to APP
    
    * 'Add select language component'
    
    Co-authored-by: José Ignacio Amelivia Santiago <ohcan2@gmail.com>
    Co-authored-by: Javier Moreno López <47107967+JaMoLpE88@users.noreply.github.com>

---
+++ apps/workkit-fe/src/i18n.js
@@ -0,0 +1,43 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import XHR from 'i18next-xhr-backend';
+
+import translationCat from './locales/ca/translation.json';
+import translationEng from './locales/en/translation.json';
+import translationEsp from './locales/es/translation.json';
+import translationEus from './locales/eu/translation.json';
+import translationGal from './locales/ga/translation.json';
+
+i18n
+  .use(XHR)
+  .use(LanguageDetector)
+  .init({
+    debug: true,
+    lng: "es",
+    fallbackLng: "es",
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false // react already safes from xss
+    },
+    resources: {
+      ca: {
+        translations: translationCat
+      },
+      en: {
+        translations: translationEng
+      },
+      es: {
+        translations: translationEsp
+      },
+      eu: {
+        translations: translationEus
+      },
+      ga: {
+        translations: translationGal
+      }
+    },
+    ns: ["translations"],
+    defaultNS: "translations"
+  })
+
+export default i18n;
